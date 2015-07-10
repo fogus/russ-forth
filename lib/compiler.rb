2f0456174df8bb9a3ee15a474a0ae5a900a8ed7c
fogus <mefogus@gmail.com>
2015-07-10 10:43:21 -0400

    Starting on a compiler: compile_words

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -1,4 +1,24 @@
 class Compiler
-  def initialize    
-  end  
+  def initialize( lex )
+    @lexicon = lex
+  end
+
+  def compile_words( *words )
+    blocks = []
+
+    words.each do |word|
+      entry = resolve_word( word )
+      raise "no such word: #{word}" unless entry
+      if entry[:immediate]
+        entry[:block].call
+      else
+        blocks << entry[:block]
+      end
+    end
+
+    proc do
+      blocks.each {|b| b.call}
+    end
+  end
 end
+
