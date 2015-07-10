e5072ac516a9086089a06e8bbac5850cb375a632
fogus <mefogus@gmail.com>
2015-07-10 11:24:12 -0400

    Starting on Reader: read_from

--- lib/reader.rb
+++ lib/reader.rb
@@ -3,4 +3,26 @@ class Reader
     @input = i
     @output = o
   end
+
+  def is_space?( ch )
+    /\W/ =~ ch.chr
+  end
+
+  def read_word
+    result = nil
+    ch = nil
+    until @input.eof?
+      ch = @input.readchar
+      if result and is_space?(ch)
+        break
+      elsif result.nil?
+        result = ch
+      else
+        result << ch
+      end
+    end
+    return result if result
+    nil
+  end
 end
+
