7f9a898419de4a11197c7430296a180e5de1c39f
fogus <mefogus@gmail.com>
2015-07-10 08:44:49 -0400

    First cut of Lexicon

---
+++ lib/lexicon.rb
@@ -0,0 +1,27 @@
+class Lexicon
+  def initialize( &block )
+    @entries = {}
+  end
+
+  def define_word( name, &block )
+    @entries[name] = { :name => name, :block => block, :immediate => false }
+    self
+  end
+
+  def define_immediate_word( name, &block )
+    @entries[name] = { :name => name, :block => block, :immediate => true }
+    self
+  end
+
+  def alias_word( name, old_name )
+    entry = self[old_name]
+    raise "No such word #{old_name}" unless entry
+    new_entry = entry.dup
+    new_entry[:name] = name
+    @entries[name] = entry
+  end
+
+  def []( name )
+    @entries[name]
+  end
+end
--- lib/russforth.rb
+++ lib/russforth.rb
@@ -1,34 +1,7 @@
 require 'pp'
 require 'dogestrings'
 require 'shufflers'
-
-class Lexicon
-  def initialize( &block )
-    @entries = {}
-  end
-
-  def define_word( name, &block )
-    @entries[name] = { :name => name, :block => block, :immediate => false }
-    self
-  end
-
-  def define_immediate_word( name, &block )
-    @entries[name] = { :name => name, :block => block, :immediate => true }
-    self
-  end
-
-  def alias_word( name, old_name )
-    entry = self[old_name]
-    raise "No such word #{old_name}" unless entry
-    new_entry = entry.dup
-    new_entry[:name] = name
-    @entries[name] = entry
-  end
-
-  def []( name )
-    @entries[name]
-  end
-end
+require 'lexicon'
 
 class Russforth
   include Verbs::Shufflers
