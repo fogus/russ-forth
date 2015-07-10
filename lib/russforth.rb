cf2d8de813b572f3154c5adbc3f4705e2a6cdaa3
fogus <mefogus@gmail.com>
2015-07-10 08:54:52 -0400

    Starting on RussForth core

--- lib/russforth.rb
+++ lib/russforth.rb
@@ -6,7 +6,13 @@ require 'lexicon'
 class Russforth
   include Verbs::Shufflers
 
-  def initialize(   )
-    
+  def initialize( s_in = $stdin, s_out = $stdout )
+    @s_in = s_in
+    @s_out = s_out
+    @lexicon = Lexicon.new
+    @stack = []
+    build_lexicon
   end
+
+  
 end
