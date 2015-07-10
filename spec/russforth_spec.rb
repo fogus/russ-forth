10668d96ee052acec9105fb236a0bfa523e72e7f
fogus <mefogus@gmail.com>
2015-07-10 09:11:46 -0400

    Building some specs for the Lexicon

--- spec/russforth_spec.rb
+++ spec/russforth_spec.rb
@@ -1,2 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+
+
+describe "Lexicon" do
+  lexicon = Lexicon.new
+
+  it "is a thing" do
+    expect(lexicon).to be_truthy
+  end
+end
+
