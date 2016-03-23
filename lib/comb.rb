e1d269388b54698d31c82bb22c164d23e7768d8e
fogus <mefogus@gmail.com>
2016-03-23 15:49:51 -0400

    Added sip

--- lib/comb.rb
+++ lib/comb.rb
@@ -2,16 +2,14 @@ require 'dogestrings'
 
 module Verbs
   module Combinators
+    def apply
+      op = @stack.pop
+      op.call
+    end
+
     def dip
       op = @stack.pop
       store = @stack.pop
-      op.call
-      @stack << store
-    end
-
-    def sip
-      op = @stack.pop
-      store = @stack.last
       op.call
       @stack << store
     end
